00490c00fedfd76c730f5455677929c8c7cb52a5
Eric Wu <ericwuyi@gmail.com>
2017-02-06 00:35:37 -0800

    Shebang and add documentation

--- git/git-dev-end.sh
+++ git/git-dev-end.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+#################################################
+# Clean up the dev branch both locally and
+# remotely.
+#################################################
+
 git checkout master
 git branch -D dev
 git push origin :dev
